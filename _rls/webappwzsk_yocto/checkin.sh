36b6cb3227c6bdbc821f2f2727b7480f685cfa76
Alexander Wirthmueller <aw@mpsitech.com>
2021-01-26 13:27:55 +0100

    added webappwzsk_yocto

---
+++ _rls/webappwzsk_yocto/checkin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# file checkin.sh
+# checkin script for Wzsk web-based UI, release webappwzsk_yocto
+# copyright: (C) 2016-2020 MPSI Technologies GmbH
+# author: Alexander Wirthmueller (auto-generation)
+# date created: 10 Jan 2021
+# IP header --- ABOVE
+
+if [ -z ${WHIZROOT+x} ]; then
+	echo "WHIZROOT is not defined. It looks as if you didn't run a Whiznium initialization script beforehand."
+	exit 1
+fi
+
+export set REPROOT=${WHIZDEVROOT}/rep
+
+cp -r * $REPROOT/wzsk/webappwzsk/
+
+rm $REPROOT/wzsk/webappwzsk/checkin.sh
---
+++ _rls/webappwzsk_yocto/checkout.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# file checkout.sh
+# checkout script for Wzsk web-based UI, release webappwzsk_yocto
+# copyright: (C) 2016-2020 MPSI Technologies GmbH
+# author: Alexander Wirthmueller (auto-generation)
+# date created: 10 Jan 2021
+# IP header --- ABOVE
+
+if [ -z ${WHIZROOT+x} ]; then
+	echo "WHIZROOT is not defined. It looks as if you didn't run a Whiznium initialization script beforehand."
+	exit 1
+fi
+
+export set WEBROOT=${SYSROOT}${WHIZROOT}/web
+
+mkdir $WEBROOT/appwzsk
+
+cp checkin.sh $WEBROOT/appwzsk/
+
+cp -r ../../webappwzsk/* $WEBROOT/appwzsk/
